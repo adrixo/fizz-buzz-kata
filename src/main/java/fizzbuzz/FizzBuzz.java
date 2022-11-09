f7acd3cdd36714b327e505b4a88f65ae61254f00
adrixo <adrianvalrom@gmail.com>
2022-11-09 16:07:39 +0100

    feat: add conversion for 1-2-4

---
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,11 @@
+package fizzbuzz;
+
+public class FizzBuzz {
+    public static String convert(int input){
+        if (input == 4)
+            return "4";
+        if (input == 2)
+            return "2";
+        return "1";
+    }
+}
--- src/test/java/fizzbuzz/fizzbuzzShould.java
+++ src/test/java/fizzbuzz/fizzbuzzShould.java
@@ -1,7 +1,30 @@
 package fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class fizzbuzzShould {
 
+    @Test
+    public void
+    convert_1_to_1() {
+        assertThat(FizzBuzz.convert(1)).isEqualTo("1");
+    }
+
+    @Test
+    public void
+    convert_2_to_2() {
+        assertThat(FizzBuzz.convert(2)).isEqualTo("2");
+    }
+
+    @Test
+    public void
+    convert_4_to_4() {
+        assertThat(FizzBuzz.convert(4)).isEqualTo("4");
+    }
+
+
+    
 }
