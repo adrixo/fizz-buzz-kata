942354092edfee50c4f85cf5886af6077aa0b754
adrixo <adrianvalrom@gmail.com>
2022-11-09 16:10:45 +0100

    refactor: remove hardcoded returns on convert (rule of 3)

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,6 @@ package fizzbuzz;
 
 public class FizzBuzz {
     public static String convert(int input){
-        if (input == 4)
-            return "4";
-        if (input == 2)
-            return "2";
-        return "1";
+        return String.valueOf(input);
     }
 }
