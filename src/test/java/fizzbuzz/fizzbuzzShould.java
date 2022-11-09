d22eb9d1da89b665b5e018a3ac795dccdaa7126b
adrixo <adrianvalrom@gmail.com>
2022-11-09 16:10:01 +0100

    test: refactor non ruled numbers

--- src/test/java/fizzbuzz/fizzbuzzShould.java
+++ src/test/java/fizzbuzz/fizzbuzzShould.java
@@ -1,30 +1,23 @@
 package fizzbuzz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class fizzbuzzShould {
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+        "1,1",
+        "2,2",
+        "4,4"
+    })
     public void
-    convert_1_to_1() {
-        assertThat(FizzBuzz.convert(1)).isEqualTo("1");
+    convert_numbers(int input, String result) {
+        assertThat(FizzBuzz.convert(input)).isEqualTo(result);
     }
 
-    @Test
-    public void
-    convert_2_to_2() {
-        assertThat(FizzBuzz.convert(2)).isEqualTo("2");
-    }
-
-    @Test
-    public void
-    convert_4_to_4() {
-        assertThat(FizzBuzz.convert(4)).isEqualTo("4");
-    }
-
-
-    
 }
